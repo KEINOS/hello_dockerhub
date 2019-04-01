81b83743760cd7fa113343175788b87054219096
KEINOS <github@keinos.com>
2019-04-01 12:30:13 +0900

    feat: add test again

--- src/check_health.sh
+++ src/check_health.sh
@@ -1,10 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+cd $(dirname $0);
 
 expect='Hello Docker Hub!'
-result=$(/bin/sh say_hello.sh)
+result=$(./say_hello.sh)
+
 if [ "${expect}" != "${result}" ]; then
     echo '[NG] Test failed.'
     exit 1
 fi
+
 echo '[OK] Test succeed.'
 exit 0
